f93a84242a6f7969505e3085cab921179437ae6a
adrian83 <adrianbrzoza1983@gmail.com>
2020-11-28 19:51:33 +0100

    impl updated

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # timelapse-pics
 
+Script for taking photos with given interval.  
+Few configuration options are available:
+- possibility of choosing camera from mounted ones
+- defining max disk space which can be used
+- defining images resolution
+- choosing location for taken pictures
+- defining interval
+
 ### Running
-1. create virtualenv `virtualenv venv`
-2. activate virtualenv `source venv/bin/activate`
-3. install sependencies `pip install -r requirements.txt`
-4. deactivate virtualenv `deactivate`
+
+#### Running with Virtualenv
+1. Create virtualenv `virtualenv venv`
+2. Activate virtualenv `source venv/bin/activate`
+3. Install dependencies `pip install -r requirements.txt`
+4. Run script `./run.sh` (or better `./run.sh --help`)
+5. Deactivate virtualenv `deactivate`
+
+#### Running without Virtualenv
+1. Install dependencies `pip install -r requirements.txt`
+2. Run script `./run.sh` (or better `./run.sh --help`)
+
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-
-
+#!/usr/bin/env python
 
 import pygame
 import pygame.camera
@@ -7,17 +6,22 @@ import pygame.camera
 import time
 from datetime import datetime
 
+import argparse
+
 from os.path import expanduser, sep, isdir, exists, isfile, join, getsize
 from os import makedirs, listdir
 
-import argparse
+
+
+mega_byte = 1024 * 1024
 
 def create_dir_if_not_exists(dir_path):
     if not exists(dir_path):
         makedirs(dir_path)
 
 def to_bytes(mega_bytes):
-    return mega_bytes * 1024 * 1024
+    return mega_bytes * mega_byte
+
 
 def default_camera():
     pygame.camera.init()
@@ -30,113 +34,114 @@ default_destination = expanduser("~") + sep + "timelapse-pics"
 default_interval = 10 # sec
 
 
-class Args:
-    pass
-
-a = Args
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--camera', default=default_camera(), help='mount point of camera (e.g. /dev/video0)')
 parser.add_argument('--resolution', default=default_resolution, help='resolution of images (e.g. 640x480)')
-parser.add_argument('--maxsize', default=str(default_maxsize), help='max size of images in MB (e.g. 100)')
-parser.add_argument('--interval', default=str(default_interval), help='default interval (in secunds) between taken images (e.g. 10')
+parser.add_argument('--maxsize', default=str(default_maxsize), type=int, help='max size of images in MB (e.g. 100)')
+parser.add_argument('--interval', default=str(default_interval), type=int, help='default interval (in secunds) between taken images (e.g. 10')
 parser.add_argument('--destination', default=default_destination, help='path to directory for storing images')
 
 
+class CmdArgs:
+    pass
 
-args = parser.parse_args(namespace=a)
+cmd_args = CmdArgs
+args = parser.parse_args(namespace=cmd_args)
 
 
+print("")
+print("Camera mounted at {0} will be used".format(cmd_args.camera))
+print("Images will have resolution {0}".format(cmd_args.resolution))
+print("Images will take at max {0}MB (+1 image)".format(cmd_args.maxsize))
+print("Images will be taken every {0}s".format(cmd_args.interval))
+print("Images will be stored at {0}".format(cmd_args.destination))
+print("")
 
-
-# print(str(a.camera))
-# print(str(a.resolution))
-# print(str(a.maxsize))
-# print(str(a.delay))
-# print(str(a.destination))
-
-print("Camera mounted at {0} will be used".format(a.camera))
-print("Images will have resolution {0}".format(a.resolution))
-print("Images will take {0}MB at max (+1 image)".format(a.maxsize))
-print("Images will be taken every {0}s".format(a.interval))
-print("Images will be stored at {0}".format(a.destination))
 
 class StorageSpaceExceeded(Exception):
 
-    def __init__(self, max_size, real_size):
-        self.max_size = max_size
-        self.real_size = real_size
+    def __init__(self, max, taken):
+        self._max = max
+        self._taken = taken
 
     def __str__(self):
-        return "no available space left"
+        return "Used {0}MB disk space from available {1}MB".format(int(self._taken/mega_byte), self._max/mega_byte)
 
 
 class SdCard:
 
     def __init__(self, size, dir):
-        self.dir = dir
-        self.size = size
-        self.used = 0
+        self._dir = dir
+        self._size = size
+        self._used = 0
         self._files = 0
 
     def store(self, img):
         number = "{:05d}".format(self._files)
         self._files += 1
-        img_path = self.dir + sep + "img" + number + ".jpg"
+        img_path = self._dir + sep + "img" + number + ".jpg"
 
         pygame.image.save(img, img_path)
-        size = getsize(img_path)
+        img_size = getsize(img_path)
 
         print("Image {0} persisted".format(img_path))
 
-        self.used += size
-        if self.used > self.size:
-            raise StorageSpaceExceeded(self.size, self.used)
+        self._used += img_size
+        if self._used > self._size:
+            raise StorageSpaceExceeded(self._size, self._used)
 
 
 class Camera:
 
-    def __init__(self, location, resolution, card):
-        self.card = card
+    def __init__(self, mount_dir, resolution, card):
+        self._card = card
 
         r_parts = resolution.split("x")
         res = (int(r_parts[0]), int(r_parts[1]))
 
-        self.cam = pygame.camera.Camera(location, res)
-        self.cam.start()
+        self._cam = pygame.camera.Camera(mount_dir, res)
+        self._cam.start()
 
     def make_image(self):
-        img = self.cam.get_image()
-        self.card.store(img)
+        img = self._cam.get_image()
+        self._card.store(img)
 
 
 class TimelapseMaker:
 
     def __init__(self, camera, interval):
-        self.camera = camera
-        self.interval = interval
+        self._camera = camera
+        self._interval = interval
 
     def take_timelapse_images(self):
         while True:
-            self.camera.make_image()
-            time.sleep(self.interval)
+            self._camera.make_image()
+            time.sleep(self._interval)
 
 
 
-sd_card_location = a.destination + sep + datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+sd_card_location = cmd_args.destination + sep + datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
 
-create_dir_if_not_exists(a.destination)
+create_dir_if_not_exists(cmd_args.destination)
 create_dir_if_not_exists(sd_card_location)
 
-sd_card = SdCard(to_bytes(int(a.maxsize)), sd_card_location)
+sd_card = SdCard(to_bytes(cmd_args.maxsize), sd_card_location)
 
-camera = Camera(a.camera, a.resolution, sd_card)
+camera = Camera(cmd_args.camera, cmd_args.resolution, sd_card)
 
-maker = TimelapseMaker(camera, int(a.interval))
+maker = TimelapseMaker(camera, cmd_args.interval)
 
 
 try:
     maker.take_timelapse_images()
 
+except StorageSpaceExceeded as e:
+    print("")
+    print(e)
+
 except Exception as e:
+    print("")
     print(str(e))
+
+print("EXITING...")
+print("")
